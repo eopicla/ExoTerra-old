96b234a084a6ca2bea97ddce95a3ee6ac0b7c7c7
eopicla <comma2producing@gmail.com>
2022-05-15 20:23:14 -0500

    created machine and simple packages as sub of blocks package

---
+++ src/main/java/com/faojen/exoterra/blocks/machine/crystalcatalyst/CrystalCatalystItemHandler.java
@@ -0,0 +1,35 @@
+package com.faojen.exoterra.blocks.machine.crystalcatalyst;
+
+import javax.annotation.Nonnull;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.items.ItemStackHandler;
+
+public class CrystalCatalystItemHandler extends ItemStackHandler {
+    private final CrystalCatalystBE crystalCatalystBE;
+
+    public CrystalCatalystItemHandler(CrystalCatalystBE crystalCatalystBE) {
+        super(3);
+        this.crystalCatalystBE = crystalCatalystBE;
+    }
+
+    @Override
+    protected void onContentsChanged(int slot) {
+    	crystalCatalystBE.setChanged();
+    }
+
+    @Nonnull
+    @Override
+    
+    public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
+    	
+        if (slot == CrystalCatalystBE.Slots.INPUTFUEL.getId() && (! stack.is(Items.DIAMOND) && stack.getCount() == 1))
+            return stack;
+        
+        if (slot == CrystalCatalystBE.Slots.INPUTGHAST.getId() && (! stack.is(Items.GHAST_TEAR) && stack.getCount() == 1))
+            return stack;
+
+        return super.insertItem(slot, stack, simulate);
+    }
+}
---
+++ src/main/java/com/faojen/exoterra/blocks/simple/compowerbank/CommonPowerBankBlock.java
@@ -0,0 +1,126 @@
+package com.faojen.exoterra.blocks.simple.compowerbank;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import com.faojen.exoterra.setup.Registration;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.Containers;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.network.NetworkHooks;
+
+
+public class CommonPowerBankBlock extends Block implements EntityBlock {
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
+    public CommonPowerBankBlock() {
+        super(Properties.of(Material.STONE).strength(2f));
+
+        registerDefaultState(getStateDefinition().any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder);
+        builder.add(FACING);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
+        return level.isClientSide() ? null : CommonPowerBankBE::ticker;
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+        BlockEntity te = builder.getOptionalParameter(LootContextParams.BLOCK_ENTITY);
+
+        List<ItemStack> drops = super.getDrops(state, builder);
+        if (te instanceof CommonPowerBankBE) {
+        	CommonPowerBankBE tileEntity = (CommonPowerBankBE) te;
+        	CompoundTag tag = new CompoundTag();
+        	
+            drops.stream()
+                    .filter(e -> e.getItem() instanceof CommonPowerBankItem)
+                    .findFirst() 
+                    .ifPresent(e -> e.setTag(tag));
+        	
+            drops.stream()
+                    .filter(e -> e.getItem() instanceof CommonPowerBankItem)
+                    .findFirst()
+                    .ifPresent(e -> e.getOrCreateTag().putInt("energy", tileEntity.energyStorage.getEnergyStored()));
+        }
+
+        return drops;
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public void onRemove(BlockState state, Level worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
+        if (newState.getBlock() != this) {
+            BlockEntity tileEntity = worldIn.getBlockEntity(pos);
+            if (tileEntity != null) {
+                LazyOptional<IItemHandler> cap = tileEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY);
+                cap.ifPresent(handler -> {
+                    for (int i = 0; i < handler.getSlots(); i++)
+                        Containers.dropItemStack(worldIn, pos.getX(), pos.getY(), pos.getZ(), handler.getStackInSlot(i));
+                });
+            }
+            super.onRemove(state, worldIn, pos, newState, isMoving);
+        }
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level worldIn, BlockPos pos, Player player, InteractionHand hand, BlockHitResult blockRayTraceResult) {
+        // Only execute on the server
+        if (worldIn.isClientSide)
+            return InteractionResult.SUCCESS;
+
+        BlockEntity te = worldIn.getBlockEntity(pos);
+        if (!(te instanceof CommonPowerBankBE))
+            return InteractionResult.FAIL;
+
+        NetworkHooks.openGui((ServerPlayer) player, (MenuProvider) te, pos);
+        return InteractionResult.SUCCESS;
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return Registration.COMMON_POWER_BANK_BE.get().create(pos, state);
+    }
+}
