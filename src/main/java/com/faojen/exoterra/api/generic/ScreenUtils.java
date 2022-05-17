f14a718955b3d8f5dd603defe806463602c9a177
eopicla <comma2producing@gmail.com>
2022-05-16 21:20:53 -0500

    Create ScreenUtils.java

---
+++ src/main/java/com/faojen/exoterra/api/generic/ScreenUtils.java
@@ -0,0 +1,22 @@
+package com.faojen.exoterra.api.generic;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.network.chat.TranslatableComponent;
+
+public class ScreenUtils {
+
+    private void drawTranslatableComponent(PoseStack stack, TranslatableComponent component, int suffix, int x, int y, int scale){
+        // stack.pushPose();
+
+
+    }
+
+    /**
+     * this.font.draw(stack,
+     * new TranslatableComponent("screen.exoterra.accumulator_progress",
+     * MagicHelpers.withSuffix(this.container.getFilterProgress()*100/512)), testX, scaleY(46), Color.WHITE.getRGB());
+     *
+     *
+     */
+
+}
