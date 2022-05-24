7f3c5d3dd25501832ada60419ded382d48f5b11f
eopicla <comma2producing@gmail.com>
2022-05-24 17:18:16 -0500

    Update PureStellarCore.java
    Added basic item properties, testing foil boolean

--- src/main/java/com/faojen/exoterra/items/basic/PureStellarCore.java
+++ src/main/java/com/faojen/exoterra/items/basic/PureStellarCore.java
@@ -1,15 +1,24 @@
 package com.faojen.exoterra.items.basic;
 
 import com.faojen.exoterra.setup.ModSetup;
+import com.faojen.exoterra.setup.Registration;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 
 public class PureStellarCore extends Item {
 
     public PureStellarCore() {
         super(new Item.Properties()
-                .tab(ModSetup.ITEM_GROUP));
+                .tab(ModSetup.ITEM_GROUP)
+                .rarity(Rarity.EPIC)
+                .stacksTo(1)
+                .craftRemainder(Registration.INF_STELLAR_CORE.get()));
 
         //static String STELLAR_BURN_TIME = "1600";
+    }
+
+    public boolean isFoil(){
+        return true;
     }
 
 }
