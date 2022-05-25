71f2bdc03440de0b852da4ed01ea5a13f85d2188
eopicla <comma2producing@gmail.com>
2022-05-25 17:23:56 -0500

    Update SentientCore.java
    Fixed issue where if you have more than one sentient core, the intelligence level breaks. but its fixed now.

--- src/main/java/com/faojen/exoterra/items/basic/SentientCore.java
+++ src/main/java/com/faojen/exoterra/items/basic/SentientCore.java
@@ -4,6 +4,7 @@ import com.faojen.exoterra.api.generic.ScreenUtils;
 import com.faojen.exoterra.setup.ModSetup;
 import com.faojen.exoterra.setup.Registration;
 import com.faojen.exoterra.utils.MagicHelpers;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
@@ -21,44 +22,40 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-// TODO: THIS ALL WORKS!! except if there is more than one sentient core in the players inventory. maybeeeee only allow one? or maybe fix it. whichever is easier.
-// Also thinking that we might be able to set the tag once instead of every tick. check if there is an onpickup or oninventorychanged event ???
-
 public class SentientCore extends Item {
-
-    private int intelligence;
-    public int getIntelligence() {
-        return this.intelligence;
+    private int iterate;
+    public int getIntelligence(ItemStack pStack) {
+        return pStack.getTag().getInt("level");
     }
 
-    public void setIntelligence(int intelligence) {
-        this.intelligence = intelligence;
+    public void setIntelligence(int intelligence, ItemStack pStack) {
+        pStack.getTag().putInt("level", intelligence);
     }
 
     @Override
     public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
-        this.intelligence = pStack.getTag().getInt("level");
 
     }
 
     @Override
     public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
 
-        if(this.intelligence == 69) {
-            pTooltipComponents
-                    .add(new TextComponent("nice."));
-        }
+        if(pStack.hasTag()) {
+            if (pStack.getTag().getInt("level") == 69) {
+                pTooltipComponents
+                        .add(new TextComponent("nice."));
+            }
 
-        if (this.intelligence < 100) {
-            pTooltipComponents
-                    .add(new TranslatableComponent("itemHover.exoterra.intelligence", MagicHelpers.withSuffix(intelligence)));
-        }
+            if (pStack.getTag().getInt("level") < 100) {
+                pTooltipComponents
+                        .add(new TranslatableComponent("itemHover.exoterra.intelligence", MagicHelpers.withSuffix(pStack.getTag().getInt("level"))));
+            }
 
-        if (this.intelligence == 100) {
-            pTooltipComponents
-                    .add(new TextComponent("\u00A72Intelligent.\u00A7r"));
+            if (pStack.getTag().getInt("level") == 100) {
+                pTooltipComponents
+                        .add(new TextComponent("\u00A72Intelligent.\u00A7r"));
+            }
         }
-
         super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 
@@ -66,9 +63,10 @@ public class SentientCore extends Item {
     public InteractionResult interactLivingEntity(ItemStack pStack, Player pPlayer, LivingEntity pInteractionTarget, InteractionHand pUsedHand) {
 //        return super.interactLivingEntity(pStack, pPlayer, pInteractionTarget, pUsedHand);
 
-        if(pInteractionTarget.isBaby() && this.intelligence < 100){
+        if(pInteractionTarget.isBaby() && pStack.getTag().getInt("level") < 100 && !pPlayer.getLevel().isClientSide()){
+            iterate = pStack.getTag().getInt("level") + 1;
             pInteractionTarget.kill();
-            pStack.getTag().putInt("level", this.intelligence + 1);
+            pStack.getTag().putInt("level", iterate);
 
             return InteractionResult.PASS;
         } else
